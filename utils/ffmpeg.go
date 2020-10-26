a78528c6321dcce2001ad7ef4748bf4d59075049
Ben Lavery-Griffiths <benjaminl@backbase.com>
2020-10-26 10:21:01 +0000

    Switch to libui
    GTK has proven nice, but difficult to package for non-Linux OSs nicely.
    The switch to libui (via the ui package) should allow for cross-compiling nicely.

--- utils/ffmpeg.go
+++ utils/ffmpeg.go
@@ -6,7 +6,11 @@ import (
 )
 
 func ProcessVideo(image string, audio string, output string) bool {
-	command := exec.Command("/usr/bin/ffmpeg", "-i", audio, "-i", image, "-filter_complex", "[0:a]aformat=channel_layouts=mono,showwaves=s=1080x600:mode=line:colors=orange[sw];[1][sw]overlay=(W-w)/2:(H-h)/2:format=auto,format=yuv420p[v]", "-map", "[v]", "-map", "0:a", "-vcodec", "libx265", "-crf", "28", output)
+	if !whereis("ffmpeg") {
+		return false
+	}
+
+	command := exec.Command("ffmpeg", "-i", audio, "-i", image, "-filter_complex", "[0:a]aformat=channel_layouts=mono,showwaves=s=1080x600:mode=line:colors=orange[sw];[1][sw]overlay=(W-w)/2:(H-h)/2:format=auto,format=yuv420p[v]", "-map", "[v]", "-map", "0:a", "-vcodec", "libx265", "-crf", "28", output)
 	out, err := command.Output()
 	if err != nil {
 		log.Println("ffmpeg failed")
@@ -15,5 +19,13 @@ func ProcessVideo(image string, audio string, output string) bool {
 		return false
 	}
 
+	return true
+}
+
+func whereis(name string) bool {
+	cmd := exec.Command("/bin/sh", "-c", "command -v "+name)
+	if err := cmd.Run(); err != nil {
+		return false
+	}
 	return true
 }
